4e98c2dc28e66801458bb157a637a5c5864d7ab7
Octavio Lucardi Fierro <octaviolf2016@gmail.com>
2023-11-25 22:31:00 -0300

    Estilos

--- react-workspace-final/src/Componentes/Notas/Nota.jsx
+++ react-workspace-final/src/Componentes/Notas/Nota.jsx
@@ -1,17 +1,50 @@
 import React from 'react'
 import { useState } from 'react'
 import './nota.css'
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { MdDeleteForever } from "react-icons/md";
 
 const Nota  = ({task, deleteTask}) =>{
     const {title, description, createdAt, id} = task
 
+    const [terminado, SetTerminado]= useState(false)
+
+    const handleTerminado = () =>{
+        SetTerminado(!terminado)
+    }
+
+
     return (
         <div className='nota'>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <span>{createdAt}</span>
-            <button onClick={() => deleteTask(task.id)}> Terminar</button>
-            <hr />
+            <div className='todoIzq'>
+                {
+                    terminado
+                    ?   <div className='containerBotones'><h2><IoMdCheckmarkCircleOutline />  <strike>{title}</strike></h2>
+                        <button className='deleteButton' onClick={() => deleteTask(task.id)}> <MdDeleteForever /></button></div>
+
+                    : <div className='containerBotones'><h2>{title}</h2> 
+                    <button className='deleteButton' onClick={() => deleteTask(task.id)}> <MdDeleteForever /></button></div>
+                }
+                
+
+                {
+                    terminado
+                    ? <p><strike>{description}</strike></p> 
+                    : <p>{description}</p>
+                }
+                
+                <div className='TaskButton'>
+                    <span>{createdAt}</span>
+                    {
+                        terminado
+                        ?<button  onClick={handleTerminado}>No lo termine</button>
+                        : <button  onClick={handleTerminado}>Tarea hecha</button>
+                    
+                    }
+                     
+                </div>
+                    
+            </div>
         </div>
     )
 }
